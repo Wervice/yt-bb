0954a57bd42b80e820685d64d12efb3651a3cd80
wervice <wervice@proton.me>
2023-09-09 18:33:55 +0200

    Update block.js

--- block.js
+++ block.js
@@ -1,5 +1,6 @@
 // Copy and paste in developer tools console or to the code filed of the YouTube Enhancer Addon.
-function byeit() {
+
+/* function byeit() {
 try {
     document.getElementsByClassName("ytd-popup-container")[0].remove() }
 catch {
@@ -15,6 +16,7 @@ catch {
 
 iid = setInterval(byeit, 100)
 setTimeout(function () { clearInterval(iid) }, 5000)
+*/
 
 function skip() { if (document.getElementsByClassName("ytp-ad-player-overlay-flyout-cta-rounded")[0] != null) { document.getElementsByTagName("video")[0].currentTime = 100 } }
 setInterval(skip, 100)
