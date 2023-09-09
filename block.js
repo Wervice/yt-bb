845c381e32f4fdbbe674d13545027ee4491ddfe7
wervice <wervice@proton.me>
2023-09-09 18:28:08 +0200

    Update block.js

--- block.js
+++ block.js
@@ -15,3 +15,6 @@ catch {
 
 iid = setInterval(byeit, 100)
 setTimeout(function () { clearInterval(iid) }, 5000)
+
+function skip() { if (document.getElementsByClassName("ytp-ad-player-overlay-flyout-cta-rounded")[0] != null) { document.getElementsByTagName("video")[0].currentTime = 100 } }
+setInterval(skip, 100)
